b1633c3225269fac5947f538d952d05f4cbfcee9
Rushikesh Naiknaware <rushikesh.naiknaware@atmecs.com>
2017-04-24 12:59:39 +0530

    Adding dummy files to keep folder in place

---
+++ src/main/java/com/rushi/Simple.java
@@ -0,0 +1,13 @@
+package com.rushi;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		System.out.println("-----------------------------");
+		System.out.println("Welcome Rushikesh!!");
+		System.out.println("Good Morning");
+		System.out.println("-----------------------------");
+
+	}
+
+}
