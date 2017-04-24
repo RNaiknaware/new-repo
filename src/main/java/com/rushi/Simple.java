82882f7bf49a2a28cc333ef0380762e8794f2bcc
Rushikesh Naiknaware <rushikesh.naiknaware@atmecs.com>
2017-04-24 17:04:17 +0530

    Save changes to SImple

--- src/main/java/com/rushi/Simple.java
+++ src/main/java/com/rushi/Simple.java
@@ -2,12 +2,16 @@ package com.rushi;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Simple {
 
+	public Simple(){
+		System.out.println("Simple Constructor!!");
+		}
+
 	public void greet(){
 			List<String> lst = new ArrayList<String>();
-			
 			
 			lst.add("Hello ");
 			lst.add("Maven");
@@ -16,15 +20,16 @@ public class Simple {
 				System.out.println("Greeting "+ele);
 			}
 			
-		
 			System.out.println("-----------------------------");
 	}
 
-	public Simple(){
-		System.out.println("Simple Constructor!!");
+	public int count_words(String words){
+		String[] sepratewords	=	StringUtils.split(words,' ');
+		return (sepratewords == null) ? 0 : sepratewords.length;
 	}
-
+	
 	public static void main(String[] args) {
+		
 		System.out.println("-----------------------------");
 		System.out.println("Welcome Rushikesh!!");
 		System.out.println("Good Morning");
@@ -32,7 +37,7 @@ public class Simple {
 		Simple samp = new Simple();
 		samp.greet();
 		
-		
+		System.out.println("count : "+samp.count_words("Hello Rushi How are you?"));
 		
 	}
 
