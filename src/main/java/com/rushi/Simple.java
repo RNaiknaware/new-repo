f2fbfa0ee7c29b843878a61c316d58b66e0e8865
Rushikesh Naiknaware <rushikesh.naiknaware@atmecs.com>
2017-04-24 15:29:44 +0530

    Adding Changes to repository

--- src/main/java/com/rushi/Simple.java
+++ src/main/java/com/rushi/Simple.java
@@ -1,13 +1,39 @@
 package com.rushi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Simple {
+
+	public void greet(){
+			List<String> lst = new ArrayList<String>();
+			
+			
+			lst.add("Hello ");
+			lst.add("Maven");
+			
+			for(String ele : lst){
+				System.out.println("Greeting "+ele);
+			}
+			
+		
+			System.out.println("-----------------------------");
+	}
+
+	public Simple(){
+		System.out.println("Simple Constructor!!");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("-----------------------------");
 		System.out.println("Welcome Rushikesh!!");
 		System.out.println("Good Morning");
 		System.out.println("-----------------------------");
-
+		Simple samp = new Simple();
+		samp.greet();
+		
+		
+		
 	}
 
 }
